ec8133a564ec9e2c2f68bb5ecd2f7f02ca59838f
Sandeep Raj <rudra.utpal@gmail.com>
2020-07-27 20:36:39 +0530

    test auto deploy

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 // auth middleware, here we can use combo of JWT & Passport.
 module.exports = {
     auth(req, res, next){
-        //extract jwt token, aithenticate & decode.
+        //extract jwt token, authenticate & verify.
         //check for roles. throws 401 if it doesn't match
         // if all OK call next middleware.
         console.log('auth middleware called.');
