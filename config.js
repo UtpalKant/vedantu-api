b6dccdaa8c2d25f6b3d87dca8ce5e7c53368c3a7
Sandeep Raj <rudra.utpal@gmail.com>
2020-07-27 11:00:35 +0530

    config

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module.exports = {
-        // mongodbUrl: "mongodb://127.0.0.1:27017/",
-        mongodbUrl: `mongodb+srv://utest01:P@ssw0rd@cluster0-dgqse.mongodb.net/shopDB?retryWrites=true&w=majority`,
+        // local
+        mongodbUrl: "mongodb://127.0.0.1:27017/",
+        //cloud
+        //mongodbUrl: `mongodb+srv://utest01:P@ssw0rd@cluster0-dgqse.mongodb.net/shopDB?retryWrites=true&w=majority`,
+        // db name
         defaultdb: "shopDB"
 }
